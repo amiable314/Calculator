12c03df49053e9c631abd3588dbe907870294fb9
WenTing <wenting@WenTingde-MacBook-Pro.local>
2017-10-01 15:18:12 +0800

    01

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,16 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var display: UILabel?
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        let textCurrentlyInDisplay = display!.text!
+        display!.text! = textCurrentlyInDisplay + digit
+        print("\(digit) is called")
+     
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+    
+    
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
