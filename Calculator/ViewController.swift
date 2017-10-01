d307c1148a40927866c408f20719e8636379c17a
WenTing <wenting@WenTingde-MacBook-Pro.local>
2017-10-01 19:12:58 +0800

    05

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,19 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var display: UILabel?
+    @IBOutlet weak var display: UILabel!
     var userIsInTheMiddleOfTypping = false
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTypping{
-            let textCurrentlyInDisplay = display!.text!
-            display!.text! = textCurrentlyInDisplay + digit
+            let textCurrentlyInDisplay = display.text!
+            display.text! = textCurrentlyInDisplay + digit
         }else{
-            display!.text! = digit
+            display.text! = digit
             userIsInTheMiddleOfTypping = true  //new number there
         }
         //print("\(digit) is called")
      
+    }
+    
+    var displayValue : Double{
+        get{
+            return Double(display.text!)!
+        }set{
+            display.text! = String(newValue)
+        }
     }
     
     
@@ -31,10 +39,9 @@ class ViewController: UIViewController {
            userIsInTheMiddleOfTypping = false //not to add numbers after π
            switch mathematicalSymbol{
            case "π" :
-                display!.text = String(Double.pi)
+               displayValue = Double.pi
            case "√" :
-                let operand = Double(display!.text!)!
-                display!.text = String(sqrt(operand))
+               displayValue = sqrt(displayValue)
            default:
             break
             
@@ -47,13 +54,7 @@ class ViewController: UIViewController {
     
     
     
-    override func viewDidLoad() {
-      
     
-    
-    
-    }
-
    
 
 
